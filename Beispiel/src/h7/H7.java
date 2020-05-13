f7303f3751f05b498412c8e2d384e8b93970594d
Firat <Uebeyd.Firat@ottobock.com>
2020-05-13 02:51:10 +0200

    fixed Bug in creating ma List

--- Beispiel/src/h7/H7.java
+++ Beispiel/src/h7/H7.java
@@ -4,16 +4,19 @@ public class H7 {
 
 	public static void main(String[] args)
 	{
-		Mitarbeiter[] ma = new Mitarbeiter[4];
+		Mitarbeiter[] ma = new Mitarbeiter[3];
 		ma[0] = new CEO("Lou", 1, "Armonk", 1993, 3500, 20000, 30000);
 		ma[1] = new Manager("Sam", 2, "Armonk", 1983, 1500, 10000);
-	//	ma[2] = new Arbeiter("Joe", 3, "Armonk", 1973, 50, 100);
-		ma[3] = new Angestellter("Fred", 4, "Armonk", 1989, 100);
+		ma[2] = new Angestellter("Fred", 4, "Armonk", 1989, 100);
+		
 		double ts = 0;
-		for(Mitarbeiter m: ma) {
-			m.print();
-			ts += m.monatsBrutto();
+		
+		for(int i= 0; i < ma.length; i++ ) {
+			
+			ma[i].print();
+			ts = ts + ma[i].monatsBrutto();
 		}
+
 		System.out.println("Die monatlichen Lohnkosten für alle Mitarbeiter betragen " + ts);
 	}
 
