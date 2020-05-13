ff47fd5755fb68c9a56612fa8d7c14f593ea23f8
Firat <Uebeyd.Firat@ottobock.com>
2020-05-13 02:35:08 +0200

    Fix build problems

--- Beispiel/src/h7/H7.java
+++ Beispiel/src/h7/H7.java
@@ -7,7 +7,7 @@ public class H7 {
 		Mitarbeiter[] ma = new Mitarbeiter[4];
 		ma[0] = new CEO("Lou", 1, "Armonk", 1993, 3500, 20000, 30000);
 		ma[1] = new Manager("Sam", 2, "Armonk", 1983, 1500, 10000);
-		ma[2] = new Arbeiter("Joe", 3, "Armonk", 1973, 50, 100);
+	//	ma[2] = new Arbeiter("Joe", 3, "Armonk", 1973, 50, 100);
 		ma[3] = new Angestellter("Fred", 4, "Armonk", 1989, 100);
 		double ts = 0;
 		for(Mitarbeiter m: ma) {
